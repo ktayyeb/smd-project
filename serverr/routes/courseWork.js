4c52b90333aa9e54c34c4d84ee925928768a69c3
ktayyeb <khaledwaleed180@gmail.com>
2022-06-15 02:49:28 +0200

    fix backend

--- serverr/routes/courseWork.js
+++ serverr/routes/courseWork.js
@@ -66,7 +66,11 @@ router.post('/assessment',async(req,res)=>{
   //console.log(assessmentInfo);
   //console.log(course);
 
-  if(assessmentInfo.completed===assessmentInfo.completed){
+  console.log(assessmentInfo.completed)
+  console.log(assessmentInfo.completed)
+  
+  
+  if(assessmentInfo.completed===assessmentInfo.num){
     res.send({message:"You inserted all of your assessments here!"});
     
   }
@@ -195,7 +199,7 @@ router.post('/finalGrade',async (req,res)=>{
 });
 
 router.get('/assessments/:sid/:cid/:type',(req,res)=>{
-  Assessment.find({sid:req.params.sid,cid:req.params.cid,type:req.params.type},{title:1,num:1,grade:1,totalGrade:1}).sort({num:-1}).then((assessments)=> {res.send(assessments);} ).catch((e)=>{res.send(e)});
+  Assessment.find({sid:req.params.sid,cid:req.params.cid,type:req.params.type},{title:1,num:1,grade:1,totalGrade:1}).sort({num:-1}).then((assessments)=> {res.send({assessments,});} ).catch((e)=>{res.send(e)});
 
 });
 
